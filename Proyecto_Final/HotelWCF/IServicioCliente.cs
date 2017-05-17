0ab9e9aff4948491ba1549fae807a9de07cfb381
Jhan Karlo Santa María Calderón <jsantamaria@elbosque.org.pe>
2017-05-17 16:42:27 -0500

    Servicio de Cliente

---
+++ Proyecto_Final/HotelWCF/IServicioCliente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HotelWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServicioCliente" in both code and config file together.
+    [ServiceContract]
+    public interface IServicioCliente
+    {
+        [OperationContract]
+        ClienteBE DevuelveClientePorDNI(string dni);
+        [OperationContract]
+        Boolean InsertarCliente(ClienteBE objClienteBE);
+        [OperationContract]
+        Boolean ActualizarCliente(ClienteBE objClienteBE);
+    }
+    public class ClienteBE
+    {
+        [DataMember]
+        public int IdCliente { get; set; }
+        [DataMember]
+        public string ApellidoPaterno { get; set; }
+        [DataMember]
+        public string ApellidoMaterno { get; set; }
+        [DataMember]
+        public string Nombres { get; set; }
+        [DataMember]
+        public DateTime FechaNacimiento { get; set; }
+        [DataMember]
+        public string NroDocumento { get; set; }
+        [DataMember]
+        public string Correo { get; set; }
+        [DataMember]
+        public string TelefonoCasa { get; set; }
+        [DataMember]
+        public string TelefonoCelular { get; set; }
+        [DataMember]
+        public string Direccion { get; set; }
+        [DataMember]
+        public string IdUbigeo { get; set; }
+        [DataMember]
+        public byte IdTipoCliente { get; set; }
+    }
+}
---
+++ Proyecto_Final/HotelWCF/ServicioCliente.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HotelWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServicioCliente" in both code and config file together.
+    public class ServicioCliente : IServicioCliente
+    {
+        hotelproEntities MiHotel = new HotelWCF.hotelproEntities();
+        public bool ActualizarCliente(ClienteBE objClienteBE)
+        {
+            Boolean retorno = false;
+            try
+            {
+                Cliente cliente = MiHotel.Cliente.Find(objClienteBE.IdCliente);
+
+                cliente.Apellido_Paterno = objClienteBE.ApellidoPaterno;
+                cliente.Apellido_Materno = objClienteBE.ApellidoMaterno;
+                cliente.Nombres = objClienteBE.Nombres;
+                cliente.NroDocumento = objClienteBE.NroDocumento;
+                cliente.Correo = objClienteBE.Correo;
+                cliente.Direccion = objClienteBE.Direccion;
+                cliente.Fecha_Nacimiento = objClienteBE.FechaNacimiento;
+                cliente.id_Ubigeo = objClienteBE.IdUbigeo;
+                cliente.id_Tipo_Cliente = objClienteBE.IdTipoCliente;
+                cliente.Telefono_Casa = objClienteBE.TelefonoCasa;
+                cliente.Telefono_Celular = objClienteBE.TelefonoCelular;
+                MiHotel.SaveChanges();
+                retorno = true;
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return retorno;
+        }
+
+        public ClienteBE DevuelveClientePorDNI(string dni)
+        {
+            ClienteBE objClienteBE = null;
+            try
+            {
+                Cliente cliente = (from o in MiHotel.Cliente
+                                   where o.NroDocumento == dni
+                                   select o).First();
+                if (cliente != null)
+                {
+                    objClienteBE = new ClienteBE();
+                    objClienteBE.IdCliente = cliente.id_Cliente;
+                    objClienteBE.ApellidoPaterno = cliente.Apellido_Paterno;
+                    objClienteBE.ApellidoMaterno = cliente.Apellido_Materno;
+                    objClienteBE.Nombres = cliente.Nombres;
+                    objClienteBE.NroDocumento = cliente.NroDocumento;
+                    objClienteBE.Correo = cliente.Correo;
+                    objClienteBE.Direccion = cliente.Direccion;
+                    objClienteBE.FechaNacimiento = (DateTime)cliente.Fecha_Nacimiento;
+                    objClienteBE.IdUbigeo = cliente.id_Ubigeo;
+                    objClienteBE.IdTipoCliente = (byte)cliente.id_Tipo_Cliente;
+                    objClienteBE.TelefonoCasa = cliente.Telefono_Casa;
+                    objClienteBE.TelefonoCelular = cliente.Telefono_Celular;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return objClienteBE;
+        }
+
+        public bool InsertarCliente(ClienteBE objClienteBE)
+        {
+            Boolean retorno = false;
+            try
+            {
+                Cliente cliente = new Cliente();
+                cliente.Apellido_Paterno = objClienteBE.ApellidoPaterno;
+                cliente.Apellido_Materno = objClienteBE.ApellidoMaterno;
+                cliente.Nombres = objClienteBE.Nombres;
+                cliente.NroDocumento = objClienteBE.NroDocumento;
+                cliente.Correo = objClienteBE.Correo;
+                cliente.Direccion = objClienteBE.Direccion;
+                cliente.Fecha_Nacimiento = objClienteBE.FechaNacimiento;
+                cliente.id_Ubigeo = objClienteBE.IdUbigeo;
+                cliente.id_Tipo_Cliente = objClienteBE.IdTipoCliente;
+                cliente.Telefono_Casa = objClienteBE.TelefonoCasa;
+                cliente.Telefono_Celular = objClienteBE.TelefonoCelular;
+                MiHotel.Cliente.Add(cliente);
+                MiHotel.SaveChanges();
+                retorno = true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return retorno;
+        }
+    }
+}
